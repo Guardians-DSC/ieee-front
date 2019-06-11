2c16e438c76a89e510b59564006f85b57d9172dd
nathan <nathanfpedroza@gmail.com>
2019-06-11 03:49:30 -0300

    Starting the development. AppBar added

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Before anything:
+
+### `npm install`
+### `npm install @material-ui/core`
+### `npm install @material-ui/icons`
+
+### Both as adm
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/AppBar'
+import MainBar from './components/AppBar';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <MainBar name="IEEE Task Manager 1.0"/>
         </header>
       </div>
     );
