29443200c3f7b661c4a85462216af5c6a62e5342
yovanycunha <yovany.cunha@ccc.ufcg.edu.br>
2019-06-17 09:36:35 -0300

    icons on task's card

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css';
-import {Card, Col} from 'antd';
+import {Card, Col, Icon} from 'antd';
+
+Icon.setTwoToneColor('#eb2f96');
+Icon.getTwoToneColor(); // #eb2f96
 
 export default class Task extends Component {
 
@@ -11,11 +14,19 @@ export default class Task extends Component {
         };
     };
 
+    clicou(titulo) {
+        console.log('Clicou pra editar ' + titulo);
+    }
+
+    deletar() {
+        console.log('vai deletar ? ')
+    }
+
     render() {
         return (
             <React.Fragment>
                 <Col span={4} style={{'marginBottom':'20px'}} xs={24} sm={12} md={4} >
-                    <Card title={this.props.title} >
+                    <Card title={this.props.title} hoverable={true} extra={<Icon type="edit" />} actions={[<Icon type="delete" theme="twoTone" />]}>
                         {this.props.description}
                     </Card>
                 </Col>
